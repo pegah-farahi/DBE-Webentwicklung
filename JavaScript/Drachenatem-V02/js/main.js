e1b073df2515e8161c860417642def81506c2f67
pegah-farahi <pegah.farahi@gmail.com>
2026-07-20 22:04:06 +0200

    Add Drachenatem-V02

---
+++ JavaScript/Drachenatem-V02/js/main.js
@@ -0,0 +1,8 @@
+
+let portionsmenge = 4;
+
+let zutatenMenge = 200;
+
+let berechneteMenge = zutatenMenge * portionsmenge;
+
+console.log("Berechnete Portionsmenge: " + berechneteMenge);
--- JavaScript/Drachenatem/js/main.js
+++ JavaScript/Drachenatem/js/main.js
@@ -1,8 +1,2 @@
-
 let portionsmenge = 4;
-
-let zutatenMenge = 200;
-
-let berechneteMenge = zutatenMenge * portionsmenge;
-
-console.log("Berechnete Portionsmenge: " + berechneteMenge);
+alert(portionsmenge);
